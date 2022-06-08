983567577f81874dfc9d170976708c2fa90dd0bb
Ragnar-exe <53235270+Lionjj@users.noreply.github.com>
2022-06-08 17:08:59 +0200

    updata
    -Aggiunta del bottone per il menu
    -creazione del sotto menu(da finire)

--- JavaScript/navMenu.js
+++ JavaScript/navMenu.js
@@ -2,11 +2,15 @@
 const logo = document.querySelector('.nav-icon');
 const menu = document.querySelector('.navMenu');
 const section = document.querySelector('section');
+const subMenu = document.querySelector('.subMenu');
+const itemsMenu = document.querySelector('.item_menu');
+
 let isOpen = false;
 
 if (logo){
     logo.addEventListener('click', function (){
         menu.classList.toggle('showNavMenu');
+        logo.classList.toggle('x');
         if (isOpen){
             section.classList.remove('blrText');
             isOpen = false;
@@ -14,5 +18,8 @@ if (logo){
             section.classList.add('blrText');
             isOpen = true;
         }
+        itemsMenu.addEventListener('click', function (){
+            subMenu.classList.toggle('showSubMenu');
+        })
     });
 }
