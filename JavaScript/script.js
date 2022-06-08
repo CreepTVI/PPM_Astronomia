1d0c47563542f586494ecc402453f282858e6c80
Xamtiat <61709712+dinoslayer1402@users.noreply.github.com>
2022-06-08 18:17:04 +0200

    aggiornamento
    aggiunta pagina dei test con css e js, più modifiche al resto

---
+++ JavaScript/script.js
@@ -0,0 +1,135 @@
+const quizData = [
+    {
+        question: "Quale fra queste non è una minaccia per la sopravvivenza di un pianeta?",
+        a: "Un pianeta in cui la probabilità di essere abitabile è superiore rispetto alla Terra",
+        b: "Un pianeta in cui la probabilità di essere abitabile è superiore rispetto alla Terra",
+        c: "Un pianeta in cui la probabilità di essere abitabile è superiore rispetto alla Terra",
+        d: "Un pianeta in cui la probabilità di essere abitabile è superiore rispetto alla Terra",
+        correct: "a",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Cascading Style Sheets",
+        b: "Central Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "a",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "a",
+    },
+
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+    let x = Math.floor(Math.random() * 100);
+    if(x < 25)
+    {
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.a
+        b_text.innerText = currentQuizData.b
+        c_text.innerText = currentQuizData.c
+        d_text.innerText = currentQuizData.d
+        quizData[currentQuiz].correct = "a"
+    }
+
+    if(x >= 50 && x < 75) 
+    {
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.b
+        b_text.innerText = currentQuizData.a
+        c_text.innerText = currentQuizData.d
+        d_text.innerText = currentQuizData.c
+        quizData[currentQuiz].correct = "b"
+    }
+
+    if(x >= 75 && x < 100) 
+    {
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.c
+        b_text.innerText = currentQuizData.d
+        c_text.innerText = currentQuizData.a
+        d_text.innerText = currentQuizData.b
+        quizData[currentQuiz].correct = "c"
+    }
+    
+
+    if(x >= 25 && x < 50) 
+    {
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.d
+        b_text.innerText = currentQuizData.c
+        c_text.innerText = currentQuizData.b
+        d_text.innerText = currentQuizData.a
+        quizData[currentQuiz].correct = "d"
+    }
+
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button class="button button1" onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
